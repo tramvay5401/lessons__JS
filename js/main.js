2318e8cd75070189a52f6132e2b9e342b8506e1c
tramvay5401 <tramvay5401@gmail.com>
2020-01-21 12:36:29 +0200

    adedd 1 file JS

--- js/main.js
+++ js/main.js
@@ -32,39 +32,3 @@ if (time < 0) {
 
 alert(mainList.budget / 30);
 console.log(mainList);
-// if (num < 49) {
-//   console.log("not");
-// } else if (num > 100) {
-//   console.log("not");
-// } else {
-//   console.log("true");
-// }
-
-// num == 50 ? console.log("true") : console.log("not");
-
-// switch (num) {
-//   case 49:
-//     console.log("malo");
-//     break;
-//   case 100:
-//     console.log("mnogo");
-//     break;
-//   case 80:
-//     console.log("vse eche mnogo");
-//     break;
-//   case 50:
-//     console.log("v tochku");
-//     break;
-//   default:
-//     console.log("ne v etot raz");
-//     break;
-// }
-
-// do {
-//   console.log(num);
-//   num++;
-// } while (num < 55);
-
-// for (let i = 0; i < 8; i++) {
-//   console.log(i);
-// }
---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+// if (num < 49) {
+//   console.log("not");
+// } else if (num > 100) {
+//   console.log("not");
+// } else {
+//   console.log("true");
+// }
+
+// num == 50 ? console.log("true") : console.log("not");
+
+// switch (num) {
+//   case 49:
+//     console.log("malo");
+//     break;
+//   case 100:
+//     console.log("mnogo");
+//     break;
+//   case 80:
+//     console.log("vse eche mnogo");
+//     break;
+//   case 50:
+//     console.log("v tochku");
+//     break;
+//   default:
+//     console.log("ne v etot raz");
+//     break;
+// }
+
+// do {
+//   console.log(num);
+//   num++;
+// } while (num < 55);
+
+// for (let i = 0; i < 8; i++) {
+//   console.log(i);
+// }
+
+// Мое понимание callback function)
+// function learnJS(lang, callback) {
+//   console.log("я учу" + lang);
+//   callback();
+// }
+// learnJS("JavaScript", done);
+// function done() {
+//   console.log("Я закончил 3 урок ");
+// }
+
+// let num = 20;
+// function showFirstMessage(text){
+//     alert(text);
+//     let num = 20;
+// }
+// showFirstMessage("Hello World!")
+// console.log(num);
+
+// let calc = (a, b) => a + b;
+// console.log(calc(4, 5));
+// console.log(calc(4, 15));
+
+let str =
